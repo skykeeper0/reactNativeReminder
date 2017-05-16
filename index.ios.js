c900e16eb77a85a50591bdc3126dd745d02d2bdc
skykeeper0 <trungdinh0429@gmail.com>
2017-05-16 09:36:29 -0700

    set up ListView

--- index.ios.js
+++ index.ios.js
@@ -11,25 +11,16 @@ import {
   Text,
   View
 } from 'react-native';
-import TaskList from './app/components/TasksList'
+import TasksList from './app/components/TasksList'
 
 export default class Tasks extends Component {
   render() {
     return (
       <View>
-        <TaskList />
+        <TasksList />
       </View>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-// });
 
 AppRegistry.registerComponent('Tasks', () => Tasks);
