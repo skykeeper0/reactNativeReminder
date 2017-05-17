23410b0136980154f1acd6b4bad647bd8f36e649
skykeeper0 <trungdinh0429@gmail.com>
2017-05-16 17:12:03 -0700

    add async and await to updateList

--- app/components/TasksList/index.js
+++ app/components/TasksList/index.js
@@ -1,65 +1,62 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-import { 
-  ListView, 
-  Text, 
-  TextInput, 
-  View,
+import {
+  ListView,
+  Text,
+  TextInput,
   AsyncStorage,
+  View,
 } from 'react-native'
 
-import styles from './styles';
+import styles from './styles'
 
 export default class TasksList extends Component {
   constructor() {
     super()
 
     this.state = {
-      ds: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => r1 !== r2
-      }),
-      listOfTasks: [],
-      text:'',
+      ds: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+      listOfTask: [],
+      text: ''
     }
   }
 
   componentDidMount() {
-    this._updateList();
+    this._updateList()
   }
 
-  _changeTextInputValue(text) {
+  _changeTextValue(text) {
     this.setState({
       text
     })
   }
 
   async _addTask() {
-    const newListOfTasks = [...this.state.listOfTasks, this.state.text]
-    await AsyncStorage.setItem('listOfTasks',JSON.stringify(newListOfTasks))
+    const newList = [...this.state.listOfTask, this.state.text]
 
-    this._updateList();
+    await AsyncStorage.setItem('listOfTask', JSON.stringify(newList))
+
+    this._updateList()
   }
 
   async _updateList() {
-    let response = await AsyncStorage.getItem('listOfTasks');
-    let listOfTasks = await JSON.parse(response) || [];
+    const response = await AsyncStorage.getItem('listOfTask')
+    const newList = JSON.parse(response) || []
 
     this.setState({
-      listOfTasks
+      listOfTask: newList,
+      text:''
     })
-
-    this._changeTextInputValue('')
   }
 
   render() {
-    const dataSource = this.state.ds.cloneWithRows(this.state.listOfTasks)
+    const dataSource = this.state.ds.cloneWithRows(this.state.listOfTask)
     return (
-      <View> 
+      <View>
         <TextInput
           autoCorrect={false}
-          onChangeText={text => this._changeTextInputValue(text)}
+          onChangeText={text => this._changeTextValue(text)}
           onSubmitEditing={() => this._addTask()}
-          returnKeyType={'done'}
           style={styles.textInput}
           value={this.state.text}
         />
