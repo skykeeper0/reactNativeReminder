b56b32b45e366f54fc9177d0b8cbf16b2a40112e
skykeeper0 <trungdinh0429@gmail.com>
2017-05-18 23:01:54 -0700

    add moment and the formatedDate

--- app/components/EditTask/index.js
+++ app/components/EditTask/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 
 import {
   Text,
@@ -20,17 +21,30 @@ export default class EditTask extends Component {
 
   _onDateChange(date) {
     this.setState({
-      date
+      date,
+      expanded : true,
+      formatedDate: this._formatDate(date)
     })
   }
 
+  _formatDate(date) {
+    return moment(date).format('lll')
+  }
+
+  _getComponentDimensions(event) {
+    console.log(event.nativeEvent.layout)
+  }
+
   render() {
+    const noDueDateTitle = "Set Reminder"
+    const dueDateSetTitle = "Due on " + this.state.formatedDate
     return (
       <View style={ styles.editTaskContainer }>
-        <ExpandableCell title={ 'Due on' }>
+        <ExpandableCell title={ this.state.dateSelected ? dueDateSetTitle : noDueDateTitle }>
           <DatePickerIOS
             date={ this.state.date }
             onDateChange={date => this._onDateChange(date)}
+            onLayout={event => this._getComponentDimensions(event)}
             style={ styles.datePicker }
           />
         </ExpandableCell>
